c44625bf6f9b4413717cacaa76085779a3fbdbcd
zhangchao1x <438078478@qq.com>
2020-03-02 20:26:05 +0800

    [feat]实现项目经理对项目权限列表的CRUD

--- src/main/java/com/ecnu2020/achieveit/MyMapper.java
+++ src/main/java/com/ecnu2020/achieveit/MyMapper.java
@@ -2,7 +2,7 @@ package com.ecnu2020.achieveit;
 
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
-// test
+
 public interface MyMapper<T> extends Mapper<T>,MySqlMapper<T> {
     
 }
